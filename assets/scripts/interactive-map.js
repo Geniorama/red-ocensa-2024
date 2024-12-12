347c02bdfaab83eb8f5d2f3e63551d8918703e6a
angelpublicista <angelpublicista.1@gmail.com>
2024-12-12 16:07:25 -0500

    feat: menu interactive

--- assets/scripts/interactive-map.js
+++ assets/scripts/interactive-map.js
@@ -28,3 +28,46 @@ if (containersDots && buttonsInteractive) {
     });
   });
 }
+
+const menuInteractive = document.getElementById('menu-map')
+const mapInteractiveContainer = document.getElementById('container-map-terminal-2024')
+const columnInfo = document.getElementById('col-data-map')
+const cardsInfo = columnInfo.querySelectorAll('.card-interactive-icons')
+
+if(menuInteractive){
+  const itemsMenu = menuInteractive.querySelectorAll('.link-menu')
+  itemsMenu.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault()
+      const target = link.dataset.target;
+      if(target){
+        const containerSelected = document.getElementById(target);
+        
+        if (containerSelected) {
+          containerSelected.classList.toggle("d-none");
+
+          if (containerSelected.classList.contains("d-none")) {
+            link.classList.remove("active");
+          } else {
+            link.classList.add("active");
+          }
+        } else {
+          console.log("No existe el contenedor con el id");
+        }
+
+        if(cardsInfo){
+          cardsInfo.forEach(card => {
+            if(!card.classList.contains('d-none')){
+              columnInfo.classList.add('col')
+              mapInteractiveContainer.style.margin = "unset"
+            } else {
+              columnInfo.classList.remove('col')
+              mapInteractiveContainer.style.margin = "0 auto"
+            }
+          })
+        }
+      }
+    })
+  });
+}
+
