559a99bd4fd77025b9a400f07b268c1920a874a3
angelpublicista <angelpublicista.1@gmail.com>
2024-12-11 21:47:41 -0500

    feat: hover color icons

--- assets/scripts/interactive-map.js
+++ assets/scripts/interactive-map.js
@@ -1,24 +1,30 @@
-const containersDots = document.querySelectorAll('.dots-map-2024')
-const buttonsInteractive = document.querySelectorAll('.card-interactive-icons__container')
+const containersDots = document.querySelectorAll(".dots-map-2024");
+const buttonsInteractive = document.querySelectorAll(
+  ".card-interactive-icons__container"
+);
 
-if(containersDots && buttonsInteractive){
-    buttonsInteractive.forEach(button => {
-        button.addEventListener('click', (e) => {
-            e.preventDefault();
-            const target = button.dataset.target
-            if(target){
-                const containerSelected = document.getElementById(target)
-                containerSelected.classList.toggle('d-none')
+if (containersDots && buttonsInteractive) {
+  buttonsInteractive.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      const target = button.dataset.target;
+      if (target) {
+        const containerSelected = document.getElementById(target);
+        if (containerSelected) {
+          containerSelected.classList.toggle("d-none");
 
-                // Comprobar visibilidad de los puntos
-                if(containerSelected.classList.contains('d-none')){
-                    button.classList.remove('active')
-                } else {
-                    button.classList.add('active')
-                }
-            } else {
-                console.log('No hay un target asignado')
-            }
-        })
+          // Comprobar visibilidad de los puntos
+          if (containerSelected.classList.contains("d-none")) {
+            button.classList.remove("active");
+          } else {
+            button.classList.add("active");
+          }
+        } else {
+          console.log("No existe el contenedor con el id");
+        }
+      } else {
+        console.log("No hay un target asignado");
+      }
     });
+  });
 }
